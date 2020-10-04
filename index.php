7d34e32194c8436448345ecc7df0a891aa5f9135
Em1tt <55552821+Em1tt@users.noreply.github.com>
2020-10-04 11:22:44 +0200

    added php

---
+++ index.php
@@ -0,0 +1,45 @@
+﻿<!--<!DOCTYPE html>
+<html lang="en">
+
+</html>
+<head>
+    <link rel='shortcut icon' type='image/x-icon' href='favicon.ico' />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/styles.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>Amethyst Craft</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script>
+        
+    
+
+    </script>
+
+</head>
+
+<body class="bodyClass col-12">
+-->
+    <?php echo "YourText";?>
+<!--
+    <div class="row">
+        <div class="topnav responsive" id="myTopnav">
+            <a href="#home" class="active col-12 col-sm-2">Home</a>
+            <a href="#news" class="col-12 col-sm-2">News</a>
+            <a href="#contact" class="col-12 col-sm-2">Contact</a>
+            <a href="#about" class="col-12 col-sm-2">About</a>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+            integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+            integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+            crossorigin="anonymous"></script>
+</body>-->
